21fcecad597aebfa0d59d605ef02d6fbc47406e9
Peihong Guo <phg@tamu.edu>
2016-10-16 00:57:04 -0500

    Initial Commit

---
+++ firstSwiftProgram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  firstSwiftProgram
+//
+//  Created by Peihong Guo on 10/16/16.
+//  Copyright © 2016 Peihong Guo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
